a438512effc499ac8b2ce46ac265f5c92c4d959a
Rylan <221165zz8@gmail.com>
2026-03-18 00:02:41 +0800

    Changes

--- Source/Demo/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Demo/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,10 +3,22 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 
+#include "PlayerGameplayTags.h"
+#include "AbilitySystem/PlayerAttributeSet.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
 }
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
+	
+	UPlayerAttributeSet	 *AS = CastChecked<UPlayerAttributeSet>(AttributeSet);
+	
+	check(AttributeInfo)
+	FAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FPlayerGameplayTags::Get().Attributes_Primary_Attack);
+	Info.AttributeValue=  AS->GetAttack();
+	AttributeInfoDelegate.Broadcast(Info);
 }
--- Source/Demo/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/Demo/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -6,16 +6,30 @@
 #include "UI/WidgetController/PlayerWidgetController.h"
 #include "AttributeMenuWidgetController.generated.h"
 
+
+struct FAttributeInfo; 
+
+class UAttributeInfoAsset; 
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSignature, const FAttributeInfo&, Info);
+
 /**
- * 
- */
+ * */
 UCLASS(BlueprintType, Blueprintable)
 class DEMO_API UAttributeMenuWidgetController : public UPlayerWidgetController
 {
 	GENERATED_BODY()
-	
-	
+    
 public:
 	virtual void BindCallbacksToDependencies() override;
 	virtual void BroadcastInitialValues() override;
+    
+	// UI 蓝图会监听这个委托来生成界面
+	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
+	FAttributeInfoSignature AttributeInfoDelegate;
+    
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UAttributeInfoAsset> AttributeInfo;
 };
